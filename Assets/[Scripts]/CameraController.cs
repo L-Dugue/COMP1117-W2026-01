9caa5183f9f29d3c5b74c40fc7ca5f240c3309b0
L-Dugue <ldugue2007@outlook.com>
2026-01-13 12:26:34 -0500

    [ADD] Added logic within CameraController.cs and CameraStats.cs to handle camera movement.

--- Assets/[Scripts]/CameraController.cs
+++ Assets/[Scripts]/CameraController.cs
@@ -1,10 +1,21 @@
+using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
-{
-    private CameraStats stats = new CameraStats(); 
+{   
+    private TagCollection tagCollection;
+    private CameraStats stats;
+
+
+    void Awake()
+    {
+        tagCollection = new TagCollection();
+        stats = new CameraStats(2, GameObject.FindWithTag(tagCollection.PlayerTag));
+        stats.CameraOffset = new Vector3(0, -2, 10);
+    }
+
     void FixedUpdate()
     {
-        
+        transform.position = Vector3.SmoothDamp(transform.position, stats.targetPos, ref stats.cameraVelocity, stats.SmoothTime);
     }
 }
--- Assets/[Scripts]/CameraStats.cs
+++ Assets/[Scripts]/CameraStats.cs
@@ -1,9 +1,12 @@
+using System;
 using UnityEngine;
 
 public class CameraStats
 {
-    private float smoothTime;
 
+    private float smoothTime;
+    private GameObject trackedObj;
+    private Vector3 offset;
     public float SmoothTime
     {
         get
@@ -21,5 +24,60 @@ public class CameraStats
                 smoothTime = value;
             }
         }
+    }
+    public GameObject TrackedObj
+    {
+        get
+        {
+            return trackedObj;
+        }
+        set
+        {
+            if (value is GameObject)
+            {
+                trackedObj = value;
+            }
+            else
+            {
+                Debug.LogError("Assigned variable TrackedObj a non GameObject type. Assign a value of type GameObject");
+            }
+        }
+    }
+    public Vector3 cameraVelocity = new Vector3(0, 0, 10);
+    public Vector3 targetPos
+    {
+        get
+        {
+            return trackedObj.transform.position - CameraOffset;
+        }
+        private set
+        {
+            // CAN NOT BE
+        }
+    }
+    public Vector3 CameraOffset
+    {
+        get
+        {
+            return offset;
+        }
+        set
+        {
+            if (value.x > 25 || value.y > 25 || value.z > 25) 
+            {
+                offset = new Vector3(25, 25, 25);
+                Debug.LogWarning("Can not make offset greater than the vec3 (25, 25, 25), defaulting to: (25, 25, 25)");
+            }
+            else
+            {
+                offset = value;
+            }
+        }
+    }
+
+    public CameraStats(float _smoothTime, GameObject _trackingObj)
+    {
+        smoothTime = _smoothTime;
+        trackedObj = _trackingObj;
     }
 }
